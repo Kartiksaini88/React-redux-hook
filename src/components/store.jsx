0924d029da0da4bfcacec56207c09b36be51c942
Kartik Saini <97525903+Kartiksaini88@users.noreply.github.com>
2022-05-11 15:19:28 +0530

    Add files via upload

---
+++ src/components/store.jsx
@@ -0,0 +1,19 @@
+import { legacy_createStore as createstore,combineReducers,applyMiddleware } from "redux";
+import { loginReducer } from "./login/reducer";
+
+import { registerReducer, Registerstore } from "./register/reducer";
+
+const middleware = (store)=>(next)=>(action)=>{
+console.log("Action",action)
+next(action)
+}
+
+const rootreducer = combineReducers({
+    register:registerReducer,
+    login:loginReducer,
+    // login:loginReducer
+})
+
+
+
+export const store = createstore(rootreducer,applyMiddleware(middleware))
